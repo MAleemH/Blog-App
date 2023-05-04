5f650a454bcfdb57c257a1cf56096d191e4d1341
MAleemH <aleemhashmi4321@gmail.com>
2023-05-04 18:25:25 +0500

    Setup associations for Post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,5 @@
 class Post < ApplicationRecord
-    
+    belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+    has_many :comments, dependent: :destroy
+    has_many :likes, dependent: :destroy
 end
