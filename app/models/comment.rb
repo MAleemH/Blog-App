f13ae2ec257b4f5cefb3c8f5117ba480c245763a
MAleemH <aleemhashmi4321@gmail.com>
2023-05-04 19:41:02 +0500

    Added additional methods

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,11 @@
 class Comment < ApplicationRecord
     belongs_to :author, class_name: 'User', foreign_key: 'author_id'
     belongs_to :post, foreign_key: 'post_id'
-    counter_culture :post
+    after_save :update_comments_counter
+
+    private
+
+    def update_comments_counter
+        post.increment!(:comments_counter)
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
-    has_many :posts, dependent: :destroy
-    has_many :comments, dependent: :destroy
-    has_many :likes, dependent: :destroy
+    has_many :posts
+    has_many :comments
+    has_many :likes
+
+    def recent_post
+        posts.order(created_at: :desc).limit(3)
+    end
 end
