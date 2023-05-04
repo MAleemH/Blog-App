7bb1c3cad42bdbf306ef694fe64daa0457a59a0c
MAleemH <aleemhashmi4321@gmail.com>
2023-05-04 18:25:37 +0500

    Setup associations for Comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,5 @@
 class Comment < ApplicationRecord
-    
+    belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+    belongs_to :post, foreign_key: 'post_id'
+    counter_culture :post
 end
