c9884855def9742c8a7a787d31b17356b17e5a2b
MAleemH <aleemhashmi4321@gmail.com>
2023-05-04 19:49:50 +0500

    Fixed rubocop errors

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,11 +1,11 @@
 class Comment < ApplicationRecord
-    belongs_to :author, class_name: 'User', foreign_key: 'author_id'
-    belongs_to :post, foreign_key: 'post_id'
-    after_save :update_comments_counter
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+  belongs_to :post, foreign_key: 'post_id'
+  after_save :update_comments_counter
 
-    private
+  private
 
-    def update_comments_counter
-        post.increment!(:comments_counter)
-    end
+  def update_comments_counter
+    post.increment!(:comments_counter)
+  end
 end
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,11 +1,11 @@
 class Like < ApplicationRecord
-    belongs_to :author, class_name: 'User', foreign_key: 'author_id'
-    belongs_to :post, foreign_key: 'post_id'
-    after_save :update_likes_counter
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+  belongs_to :post, foreign_key: 'post_id'
+  after_save :update_likes_counter
 
-    private
+  private
 
-    def update_likes_counter
-        post.increment!(:likes_counter)
-    end
+  def update_likes_counter
+    post.increment!(:likes_counter)
+  end
 end
