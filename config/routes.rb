e66ca9c34624dfbce5bfcb07de24b575dc61aa7f
MAleemH <aleemhashmi4321@gmail.com>
2023-05-09 10:18:19 +0500

    Added routes for all controllers

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :users do
+    resources :posts do
+      resources :comments
+      resources :likes, only: [:create, :destroy]
+    end
+  end
 end
