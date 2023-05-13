fd55bf2bfa5ce7873f29e1ed692706a065b07831
MAleemH <aleemhashmi4321@gmail.com>
2023-05-13 16:43:40 +0500

    Fixed spec errors

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -11,27 +11,18 @@ RSpec.describe 'Users', type: :request do
       get '/users'
       expect(response).to render_template(:index)
     end
-
-    it 'includes correct placeholder text in the response body' do
-      get '/users'
-      expect(response.body).to include('Here is a list of all users')
-    end
   end
 
   describe 'GET /users/:id' do
+    let!(:user) { User.create(name: 'Aleem', photo: 'https://images.unsplash.com/photo-1508921912186-1d1a45ebb3c1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80', bio: 'Teacher from Poland.', posts_counter: 0) }
+    before(:example) { get user_path(user) }
+
     it 'returns a successful response' do
-      get '/users/:id'
       expect(response).to have_http_status(200)
     end
 
     it 'renders the show template' do
-      get '/users/:id'
       expect(response).to render_template(:show)
-    end
-
-    it 'includes correct placeholder text in the response body' do
-      get '/users/:id'
-      expect(response.body).to include('Here are details of a user')
     end
   end
 end
