dd4fc82211063a7ed62e81827d59fd76dc05b111
MAleemH <aleemhashmi4321@gmail.com>
2023-05-13 17:17:02 +0500

    Added default values

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,8 +5,8 @@ class Post < ApplicationRecord
   after_save :update_posts_counter
 
   validates :title, presence: true, length: { maximum: 250 }
-  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0, allow_nil: false, default: 0 }
+  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0, allow_nil: false, default: 0 }
 
   def recent_comments
     comments.order(created_at: :desc).limit(5)
