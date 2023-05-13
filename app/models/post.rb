e782dd93e6e75c550f082068ec47d899a4535959
MAleemH <aleemhashmi4321@gmail.com>
2023-05-13 19:30:10 +0500

    Method modified

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,8 +5,14 @@ class Post < ApplicationRecord
   after_save :update_posts_counter
 
   validates :title, presence: true, length: { maximum: 250 }
-  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0, allow_nil: false, default: 0 }
-  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0, allow_nil: false, default: 0 }
+  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+  def initialize(attributes = {})
+    super
+    self.comments_counter ||= 0
+    self.likes_counter ||= 0
+  end
 
   def recent_comments
     comments.order(created_at: :desc).limit(5)
