5bab88b66bb2f2e81baf74e4b3d86e881c30a3c1
roniy68 <ahmed.11900449@lpu.in>
2023-05-18 23:03:51 +0530

    🔥 Add app.rb

--- config/application.rb
+++ config/application.rb
@@ -7,6 +7,10 @@ require "rails/all"
 Bundler.require(*Rails.groups)
 
 module BlogApp
+
+
+
+  
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
