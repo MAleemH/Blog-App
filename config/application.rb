855bc0b7e8c71f2af4731a89b41b7cd1ed7c7ff5
roniy68 <ahmed.11900449@lpu.in>
2023-05-18 23:04:42 +0530

    🔥 Rubocop Fix

--- config/application.rb
+++ config/application.rb
@@ -7,10 +7,6 @@ require "rails/all"
 Bundler.require(*Rails.groups)
 
 module BlogApp
-
-
-
-  
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
