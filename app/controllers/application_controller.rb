0e090c7548c62ccf94598310f17951fa2c77d6a0
MAleemH <aleemhashmi4321@gmail.com>
2023-05-18 22:08:25 +0500

    Updated application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
+    protect_from_forgery with: :exception
+    before_action :authenticate_user!, :configure_permitted_parameters, if: :devise_controller?
 
+    protected
+
+    def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :bio, :photo, :password, :password_confirmation) }
+        devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :bio, :photo, :password, :current_password) }
+    end
 end
